2e62e53ccbaeae65426275c101f8d6694cc22bae
pavan92m <pavan92m@gmail.com>
2021-02-03 16:03:08 +0530

    initial commit

---
+++ EqualsListStreamApi.java
@@ -0,0 +1,28 @@
+package com.nt.steamapi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EqualsListStreamApi {
+	public static void main(String[] args) {
+		List<String> list1= new ArrayList<String>();
+		list1.add("1");
+		list1.add("pavan");
+		list1.add("pratap");
+		list1.add("Singh");
+		
+		List<String> list2= new ArrayList<String>();
+		list2.add("1");
+		list2.add("pp");
+		list2.add("ss");
+		list2.add("KK");
+
+		List<String> available = list1.stream()
+                .filter(e -> list2.contains(e))
+                .collect(Collectors.toList());
+		available.forEach(System.out::println); 
+		
+	}
+
+}
